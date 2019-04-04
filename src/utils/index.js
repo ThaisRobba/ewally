67804f3091e72db0c5333573abd93ea66ce35318
ThaisRobba <thais.robba@gmail.com>
2019-04-04 14:00:31 -0300

    Removed unused proxy URL

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,3 @@
-//TODO: Remove unused proxy
-export const proxy = "https://cors-anywhere.herokuapp.com";
 export const api = "https://apidev.ewally.com.br";
 
 export const getHeaderConfig = token => {
