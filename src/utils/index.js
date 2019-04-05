91eaa5e5e5192fa411ae96f7ab19c892fe4149a5
ThaisRobba <thais.robba@gmail.com>
2019-04-05 19:14:32 -0300

    Added transaction type utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -23,8 +23,46 @@ export const createDateRangeByDays = (start, end) => {
   return dates;
 };
 
+export const formatTransactionType = type => {
+  switch (type) {
+    case "RECEIVED_TRANSFERENCE":
+      return "Transferência recebida";
+    case "SENT_TRANSFERENCE":
+      return "Transferência para terceiros";
+    case "BANK_SLIP_DEPOSIT":
+      return "Depósito via boleto bancário";
+    case "BANK_SLIP_PAYMENT":
+      return "Pagamento via boleto bancário";
+    case "BANK_DEPOSIT":
+      return "Depósito bancário";
+    case "ECARD_CHARGE_TAX":
+      return "Taxa de uso do cartão";
+    case "ECARD_CHARGE":
+      return "Uso do cartão";
+    case "EAGENT_DEPOSIT":
+      return "Depósito de agente Ewally";
+    case "EAGENT_DEPOSIT_TAX":
+      return "Taxa de depósito de agente Ewally";
+    case "EAGENT_DEPOSIT_EAGENT":
+      return "Depósito de agente Ewally para outro agente";
+    case "EAGENT_DEPOSIT_EARNING":
+      return "Comissão de depósito de agente Ewally";
+    default:
+      return type;
+  }
+};
+
 export const formatCurrency = amount => {
-  let formatted = "R$ " + amount.toLocaleString("pt-BR");
+  let formatted;
+
+  if (amount < 0) {
+    formatted = "- R$";
+    amount = amount * -1;
+  } else {
+    formatted = "+ R$";
+  }
+
+  formatted += amount.toLocaleString("pt-BR");
 
   if (amount % 1 === 0) {
     formatted += ",00";
