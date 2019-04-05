19e0406876bedcc55b2974656a66ea81a6807d02
ThaisRobba <thais.robba@gmail.com>
2019-04-05 17:15:21 -0300

    Improved button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,15 +2,16 @@ import styled from "styled-components";
 
 const Button = styled.button`
   height: 22px;
-  background-color: #00a1b6;
-  border: none;
-  border-radius: 4px;
+  background-color: white;
+  border: 1px solid #00a1b6;
+  border-radius: 2px;
   font-family: Verdana, Geneva, Tahoma, sans-serif;
   margin: ${props => props.margin || "0px"};
-
+  color: #17626b;
   &:hover {
     background-color: #1296a7;
     cursor: pointer;
+    color: black;
   }
 
   &:disabled {
