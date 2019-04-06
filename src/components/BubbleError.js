abbb9797070f368efc86f0cf59626e2f7eb3446c
ThaisRobba <thais.robba@gmail.com>
2019-04-06 00:55:30 -0300

    Created Error Bubble Component

---
+++ src/components/BubbleError.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const BubbleError = styled.p`
+  background-color: #ec9099;
+  border-radius: 4px;
+  border: 1px solid red;
+  color: #74000b;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-size: 11px;
+  padding: 8px;
+`;
+
+export default BubbleError;
